ab759db614268318fb98c4215d14b7cec1aab75a
RK <114596148+Rakesharma319@users.noreply.github.com>
2024-08-01 19:21:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ videos_watched = st.number_input("Number of Videos Watched", min_value=0)
 quizzes_taken = st.number_input("Number of Quizzes Taken", min_value=0)
 quiz_scores = st.number_input("Average Quiz Score", min_value=0, max_value=100)
 completion_rate = st.number_input("Course Completion Rate (%)", min_value=0, max_value=100)
-device_type = st.selectbox("Device Type", ["Mobile", "Desktop", "Tablet"])
+# device_type = st.selectbox("Device Type", ["Mobile", "Desktop", "Tablet"])
+device_type = st.selectbox("Device Type", min_value=0, max_value=1)
 
 # Prediction button
 if st.button("Predict"):
